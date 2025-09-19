c330905042d2885d4ff3b58644fd27916bea6ea9
colmen4z <saircolmenarez2006@gmail.com>
2025-09-18 22:46:00 -0400

    tercer commit

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,14 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="flex min-h-screen items-center justify-center align-items">
+    <h1 class="text-3xl font-bold">
+      Hola Mundo hecho por Sair
+    </h1>
+    <button class="ml-2 rounded bg-blue-500 p-4 shadow-lg text-white hover:bg-blue-700 transition">
+      Boton de prueba
+    </button>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
