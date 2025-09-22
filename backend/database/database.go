2aeb33de696df79f3ea3ed4a30469929d1332725
colmen4z <saircolmenarez2006@gmail.com>
2025-09-22 11:21:22 -0400

    Se modifico un poco el backend

--- backend/database/database.go
+++ backend/database/database.go
@@ -1,11 +1,11 @@
-//ESTE VA A SER EL ARCHIVO POR EL CUAL NOS VAMOS A COMUNICAR CON LA BASE DE DATOS
-
 package database
 
 import (
-	"fmt"
+	"gorm.io/gorm"
 )
 
+var DB *gorm.DB
+
 func ConnectDB() {
-	fmt.Printf("DATABASE")
+
 }
