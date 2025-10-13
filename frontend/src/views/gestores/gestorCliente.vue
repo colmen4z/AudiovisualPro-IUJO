d8ccc21800cfb2b601d403eebbf7808cafee4a34
colmen4z <saircolmenarez2006@gmail.com>
2025-10-13 17:41:17 -0400

    no me acuerdo que hice xd

--- frontend/src/views/gestores/gestorCliente.vue
+++ frontend/src/views/gestores/gestorCliente.vue
@@ -14,7 +14,12 @@ import { Icon } from "@iconify/vue"
 	        	</button>
         	</div>
 
-        	<div class="border-2 border-gray-300 rounded">
+        	<div class="mb-3 flex items-center border-2 border-gray-300 rounded-lg shadow-md">
+        		<Icon icon="material-symbols:search" width="25" class="m-2 text-gray-500" />
+        		<input type="text" name="buscador" placeholder="Buscar Cliente" class="p-2 text-md w-full focus:outline-0">
+        	</div>
+
+        	<div class="border-2 border-gray-300 rounded-lg max-h-100 overflow-y-auto">
 	        	<div class="bg-white shadow-xl rounded-2xl p-3">
 			        <h2 class="text-xl font-semibold mb-3">Clientes registrados</h2>
 			        <table class="w-full text-sm text-left text-gray-700">
@@ -37,44 +42,86 @@ import { Icon } from "@iconify/vue"
 			            <tbody>
 			                <tr class="border-b hover:bg-gray-50 transition duration-150">
 			                    <td class="py-4 px-4 font-medium text-gray-900">
-			                        María Rodríguez
+			                        Sair Colmenarez
 			                    </td>
 			                    <td class="py-4 px-4 hidden sm:table-cell">
-			                        J-12345678-9
+			                        V-315118987
 			                    </td>
 			                    <td class="py-4 px-4">
-			                        maria.r@ejemplo.com
+			                        sair@ejemplo.com
 			                    </td>
 			                    <td class="py-4 px-4 hidden md:table-cell">
-			                        +58 412-1234567
-			                    </td>
-			                </tr>
-			                <tr class="bg-gray-50 border-b hover:bg-gray-100 transition duration-150">
-			                    <td class="py-4 px-4 font-medium text-gray-900">
-			                        Pedro Gómez
-			                    </td>
-			                    <td class="py-4 px-4 hidden sm:table-cell">
-			                        V-98765432-1
-			                    </td>
-			                    <td class="py-4 px-4">
-			                        pedro.g@ejemplo.com
-			                    </td>
-			                    <td class="py-4 px-4 hidden md:table-cell">
-			                        +58 414-7654321
+			                        412-2480759
 			                    </td>
 			                </tr>
 			                <tr class="border-b hover:bg-gray-50 transition duration-150">
 			                    <td class="py-4 px-4 font-medium text-gray-900">
-			                        Ana Martínez
+			                        Sair Colmenarez
 			                    </td>
 			                    <td class="py-4 px-4 hidden sm:table-cell">
-			                        G-11223344-5
+			                        V-315118987
 			                    </td>
 			                    <td class="py-4 px-4">
-			                        ana.m@ejemplo.com
+			                        sair@ejemplo.com
 			                    </td>
 			                    <td class="py-4 px-4 hidden md:table-cell">
-			                        +58 426-9988776
+			                        412-2480759
+			                    </td>
+			                </tr>
+			                <tr class="border-b hover:bg-gray-50 transition duration-150">
+			                    <td class="py-4 px-4 font-medium text-gray-900">
+			                        Sair Colmenarez
+			                    </td>
+			                    <td class="py-4 px-4 hidden sm:table-cell">
+			                        V-315118987
+			                    </td>
+			                    <td class="py-4 px-4">
+			                        sair@ejemplo.com
+			                    </td>
+			                    <td class="py-4 px-4 hidden md:table-cell">
+			                        412-2480759
+			                    </td>
+			                </tr>
+			                <tr class="border-b hover:bg-gray-50 transition duration-150">
+			                    <td class="py-4 px-4 font-medium text-gray-900">
+			                        Sair Colmenarez
+			                    </td>
+			                    <td class="py-4 px-4 hidden sm:table-cell">
+			                        V-315118987
+			                    </td>
+			                    <td class="py-4 px-4">
+			                        sair@ejemplo.com
+			                    </td>
+			                    <td class="py-4 px-4 hidden md:table-cell">
+			                        412-2480759
+			                    </td>
+			                </tr>
+			                <tr class="border-b hover:bg-gray-50 transition duration-150">
+			                    <td class="py-4 px-4 font-medium text-gray-900">
+			                        Sair Colmenarez
+			                    </td>
+			                    <td class="py-4 px-4 hidden sm:table-cell">
+			                        V-315118987
+			                    </td>
+			                    <td class="py-4 px-4">
+			                        sair@ejemplo.com
+			                    </td>
+			                    <td class="py-4 px-4 hidden md:table-cell">
+			                        412-2480759
+			                    </td>
+			                </tr>
+			                <tr class="border-b hover:bg-gray-50 transition duration-150">
+			                    <td class="py-4 px-4 font-medium text-gray-900">
+			                        Sair Colmenarez
+			                    </td>
+			                    <td class="py-4 px-4 hidden sm:table-cell">
+			                        V-315118987
+			                    </td>
+			                    <td class="py-4 px-4">
+			                        sair@ejemplo.com
+			                    </td>
+			                    <td class="py-4 px-4 hidden md:table-cell">
+			                        412-2480759
 			                    </td>
 			                </tr>
 			            </tbody>
